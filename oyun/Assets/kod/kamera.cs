d854a6d8189bc60dd1d5a1d779fee21933417b01
phenotrans <phenotrans@users.noreply.github.com>
2018-08-09 11:20:57 +0300

    Game

---
+++ oyun/Assets/kod/kamera.cs
@@ -0,0 +1,24 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kamera : MonoBehaviour {
+    Vector2 farePos;
+    Vector2 gecisPos;
+    public float hassasiyet = 0.05f;
+    public float yumusaklik = 0.5f;
+    GameObject player;
+    void Start ()
+    {
+        player = this.transform.parent.gameObject;      
+	}
+	
+	void Update ()
+    {
+        Vector2 md = new Vector2(Input.GetAxis("Axis 1"), 0);
+        md = Vector2.Scale(md, new Vector2(hassasiyet * yumusaklik, hassasiyet * yumusaklik));
+        gecisPos.x = Mathf.Lerp(gecisPos.x,md.x,1f/yumusaklik);
+        farePos += gecisPos;
+        player.transform.localRotation = Quaternion.AngleAxis(farePos.x,player.transform.up);
+    }
+}
---
+++ oyun/Assets/kod/top.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class top : MonoBehaviour {
+    
+    public bool haraket = true;
+    private Rigidbody rg;
+    public float hız;
+    public GameObject puan;
+    public Text puantext;
+    private int poin;
+
+	
+	void Start () {
+        rg = GetComponent<Rigidbody>();
+        poin = 0;
+        SetCountText();
+	}
+	
+	
+	void Update ()
+    {
+        if (Input.GetAxis("Axis 1") > 0f)
+            Debug.Log("değer geliyor");
+        rg.transform.Translate(Input.GetAxis("Axis 1") *hız*Time.deltaTime,0,0-(Input.GetAxis("Axis 2") *hız*Time.deltaTime));
+        //rg.transform.Translate(Input.acceleration.x*hız*Time.deltaTime,0,0-(Input.acceleration.z*hız*Time.deltaTime));	
+        //rg.transform.Translate(Input.GetAxis("Horizontal") * hız * Time.deltaTime, 0, Input.GetAxis("Vertical") * hız * Time.deltaTime);
+       // rg.transform.Rotate(transform.localRotation.x,transform.localRotation.y, Input.GetAxis("Vertical") * hız * Time.deltaTime);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Respawn")
+        {
+            Application.LoadLevel(0);
+        }
+        if (collision.gameObject.tag == "point")
+        {
+            Destroy(collision.gameObject);
+            poin = poin + 1;
+            Debug.Log("puan geldi");
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        puantext.text = "Puan: " + poin.ToString();
+        
+    }
+}
